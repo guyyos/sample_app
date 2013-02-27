8d3d66779e5fda82f08fd9282f4c2232a815b67f
Guy Yosiphon <guyyos@gmail.com>
2013-02-27 22:48:56 +0200

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,8 +15,8 @@ describe "Static pages" do
 		describe "for signed-in users" do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
-				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				FactoryGirl.create(:micropost, user: user, content: "Lorem")
+				FactoryGirl.create(:micropost, user: user, content: "Ipsum")
 				sign_in user
 				visit root_path
 			end
@@ -25,40 +25,48 @@ describe "Static pages" do
 					page.should have_selector("li##{item.id}", text: item.content)
 				end
 			end
+			describe "follower/following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 followers", href: followers_user_path(user)) }
+			end
 		end
-
-		describe "Help page" do
-			before { visit help_path }
-			let(:heading) { 'Help' }
-			let(:page_title) { 'Help' }
-			it_should_behave_like "all static pages"
-		end
-		describe "About page" do
-			before { visit about_path }
-			let(:heading) { 'About' }
-			let(:page_title) { 'About Us' }
-			it_should_behave_like "all static pages"
-		end
-		describe "Contact page" do
-			before { visit contact_path }
-			let(:heading) { 'Contact' }
-			let(:page_title) { 'Contact' }
-			it_should_behave_like "all static pages"
-		end
-		it "should have the right links on the layout" do
-			visit root_path
-			click_link "About"
-			page.should have_selector 'title', text: full_title('About Us')
-			click_link "Help"
-			page.should have_selector 'title', text: full_title('Help')
-			click_link "Contact"
-			page.should have_selector 'title', text: full_title('Contact')
-			click_link "Home"
-			page.should have_selector 'title', text: full_title('')
-			click_link "Sign up now!"
-			page.should have_selector 'title', text: full_title('Sign up')
-			click_link "sample app"
-			page.should have_selector 'title', text: full_title('')
-		end
+	end
+	describe "Help page" do
+		before { visit help_path }
+		let(:heading) { 'Help' }
+		let(:page_title) { 'Help' }
+		it_should_behave_like "all static pages"
+	end
+	describe "About page" do
+		before { visit about_path }
+		let(:heading) { 'About' }
+		let(:page_title) { 'About Us' }
+		it_should_behave_like "all static pages"
+	end
+	describe "Contact page" do
+		before { visit contact_path }
+		let(:heading) { 'Contact' }
+		let(:page_title) { 'Contact' }
+		it_should_behave_like "all static pages"
+	end
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About Us')
+		click_link "Help"
+		page.should have_selector 'title', text: full_title('Help')
+		click_link "Contact"
+		page.should have_selector 'title', text: full_title('Contact')
+		click_link "Home"
+		page.should have_selector 'title', text: full_title('')
+		click_link "Sign up now!"
+		page.should have_selector 'title', text: full_title('Sign up')
+		click_link "sample app"
+		page.should have_selector 'title', text: full_title('')
 	end
 end
